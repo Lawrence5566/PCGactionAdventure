5c84a0ce1f1de483acf9ae94209b5a373be46d26
Lawrence Taylor <Lawrence5566@live.co.uk>
2019-05-07 18:31:47 +0100

    Demonstration and crosshair
    -Project version for demonstration
    -Added basic crosshair that appears on lock on

--- PCG-ActionAdventure/Assets/Scripts/LockedEnemyAnalyser.cs
+++ PCG-ActionAdventure/Assets/Scripts/LockedEnemyAnalyser.cs
@@ -8,11 +8,15 @@ public class LockedEnemyAnalyser : MonoBehaviour
 	public InputHandler playerInput;
 	EnemyStates currTarget;
 
+    public GameObject crosshair;
+
 	public Text level, hp, str, def, speed, attSpeed;
 
 	void Start(){
 		if (!playerInput)
 			playerInput = FindObjectOfType<InputHandler> ();	
+		if(!crosshair) //if no crosshair attached through inspector
+			crosshair = GameObject.Find("CrosshairHolder");
 	}
 		
     void FixedUpdate()
@@ -25,13 +29,20 @@ public class LockedEnemyAnalyser : MonoBehaviour
 			def.text = currTarget.def.ToString();
 			speed.text = currTarget.speed.ToString();
 			attSpeed.text = currTarget.attackSpeed.ToString();
-		} else {
+
+            crosshair.SetActive(true); //enable crosshair
+            crosshair.transform.position = currTarget.transform.position + new Vector3(0.0f,2.0f,0.0f);
+
+        }
+
+        if(playerInput.lockon == false) {
 			level.text = "";
 			hp.text = "";
 			str.text = "";
 			def.text = "";
 			speed.text = "";
 			attSpeed.text = "";
+            crosshair.SetActive(false);
 		}
     }
 
